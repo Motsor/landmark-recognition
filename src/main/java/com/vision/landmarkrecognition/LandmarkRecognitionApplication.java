f7d586b4ac254486995ddc6b4c0ab64dd7f3d430
motsor <rostomle@outlook.com>
2019-01-07 18:02:25 +0200

    Initial commit.

---
+++ src/main/java/com/vision/landmarkrecognition/LandmarkRecognitionApplication.java
@@ -0,0 +1,14 @@
+package com.vision.landmarkrecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandmarkRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandmarkRecognitionApplication.class, args);
+    }
+
+}
+
