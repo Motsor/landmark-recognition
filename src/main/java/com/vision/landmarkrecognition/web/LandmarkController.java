c09ec00bbeb5e039527e252b6b82baa5dd7a9f76
motsor <rostomle@outlook.com>
2019-01-08 03:27:58 +0200

    Added a mapping for the image upload and the result page and injected the image analyzer.

--- src/main/java/com/vision/landmarkrecognition/web/LandmarkController.java
+++ src/main/java/com/vision/landmarkrecognition/web/LandmarkController.java
@@ -1,14 +1,35 @@
 package com.vision.landmarkrecognition.web;
 
+import com.vision.landmarkrecognition.service.ImageAnalyzer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class LandmarkController {
 
-    @GetMapping(value = "/")
-    public String index(){
+    private final ImageAnalyzer imageAnalyzer;
+
+    @Autowired
+    private LandmarkController(ImageAnalyzer imageAnalyzer) {
+        this.imageAnalyzer = imageAnalyzer;
+    }
+
+    @GetMapping(value = "/index")
+    public String index() {
         return "index";
+    }
+
+    @PostMapping(value = "/upload")
+    public String upload(MultipartFile file) {
+        return "redirect:/result";
+    }
+
+    @GetMapping(value = "/result")
+    public String result() {
+        return "result";
     }
 
 }
