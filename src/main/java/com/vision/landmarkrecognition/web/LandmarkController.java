718cfe518cb5dfd5cccafa94cc4111a2577ad8a7
motsor <rostomle@outlook.com>
2019-01-08 13:17:00 +0200

    Added attributes to the model.

--- src/main/java/com/vision/landmarkrecognition/web/LandmarkController.java
+++ src/main/java/com/vision/landmarkrecognition/web/LandmarkController.java
@@ -3,9 +3,13 @@ package com.vision.landmarkrecognition.web;
 import com.vision.landmarkrecognition.service.ImageAnalyzer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @Controller
 public class LandmarkController {
@@ -23,12 +27,14 @@ public class LandmarkController {
     }
 
     @PostMapping(value = "/upload")
-    public String upload(MultipartFile file) {
+    public String upload(MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+        redirectAttributes.addFlashAttribute("landmarkList", imageAnalyzer.detectLandmark(file));
         return "redirect:/result";
     }
 
     @GetMapping(value = "/result")
-    public String result() {
+    public String result(Model model) {
+        model.addAttribute("landmarkList", model.asMap().get("landmarkList"));
         return "result";
     }
 
